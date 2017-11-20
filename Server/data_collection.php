d33d50b03566719dbbbda29257e14d43bd0a853f
Dmitrii Ryzhov <corvin113@gmail.com>
2017-11-20 22:25:45 +0300

    Добавил кнопки запустить/прервать/продолжить обучение. Убрал мусор в виде sql бекапов.

--- Server/data_collection.php
+++ Server/data_collection.php
@@ -165,10 +165,28 @@ if(isset($_GET["p"]) && isset($_GET["t"]) && isset($_GET["g"])) {
 if (isset($_GET["start_learning"])) {
 	if ($_GET["start_learning"] == 1) {
 		//Дергаем Питон для генерации обучающей выборки.
-		exec("python ../database/generate_data.py");
+		//СНЯТЬ КОММЕНТАРИЙ.
+		//exec("python ../database/generate_data.py");
 	}
 	elseif ($_GET["start_learning"] == 2) {
 		//Дергаем Питона для запуска процесса обучения.
-		exec("python ../neurohouse.py --learn-only");
+		//СНЯТЬ КОММЕНТАРИЙ.
+		//exec("python ../neurohouse.py --learn-only");
+	}
+}
+
+if (isset($_GET["info_learn"])) {
+	//Если принят 0 - вернуть из БД, запущено ли обучение, 1 - запустить обучение, 2 - остановить обучение.
+	switch ($_GET["info_learn"]) {
+		case "0":
+			$result = _sql("SELECT state FROM public.is_learning LIMIT 1")[0]["state"];
+			echo json_encode($result, JSON_UNESCAPED_UNICODE);
+			break;
+		case "1":
+			_sql("UPDATE public.is_learning SET state = true");
+			break;
+		case "2":
+			_sql("UPDATE public.is_learning SET state = false");
+			break;
 	}
 }
--- frontend/learning.js
+++ frontend/learning.js
@@ -1,7 +1,47 @@
 var serverAddress = "../Server/data_collection.php";
+var statStartLearning = false;
 
-function load() {
-    $('.info').innerText = "Осуществляется запуск генерации обучающей выборки...";
+function onLoadPage() {
+    startLearn();
+    //Скрываем кнопку "Продолжить обучение"
+    if (!statStartLearning)
+        $('.continueLearn')["0"].style.display = "none";
+}
+
+function startLearn() {
+    //Смена надписи кнопки и класса.
+    //Стучимся на сервер и смотрим в какой стадии обучение.
+    $.getJSON(serverAddress + "?info_learn=0", function (result) {
+        console.log(result);
+        if (result === "f") {
+            //Случай с прерванным обучением.
+            $('.startStopLearn')["0"].classList.remove("btn-info");
+            $('.startStopLearn')["0"].classList.add("btn-default");
+            statStartLearning = !statStartLearning; //true
+            $('.startStopLearn')["0"].innerText = "Возобновить обучение";
+            $('.continueLearn')["0"].style.display = "block";
+        }
+        else if (result === "") {
+            $('.startStopLearn')["0"].innerText = "Перезапустить обучение";
+            statStartLearning = !statStartLearning;
+            $('.startStopLearn')["0"].classList.remove("btn-default");
+            $('.startStopLearn')["0"].classList.add("btn-info");
+        }
+        else if (result === "t") {
+            //Случай с прерванным обучением.
+            $('.startStopLearn')["0"].classList.remove("btn-info");
+            $('.startStopLearn')["0"].classList.add("btn-default");
+            statStartLearning = !statStartLearning; //true
+            $('.startStopLearn')["0"].innerText = "Прервать обучение";
+            $('.continueLearn')["0"].style.display = "block";
+        }
+    });
+
+    if (!statStartLearning) {
+        //$('.startStopLearn').classList.add()
+    }
+
+    /*$('.info').innerText = "Осуществляется запуск генерации обучающей выборки...";
     //Запрос к серверу для запуска обучающего скрипта.
     $.getJSON(serverAddress + "?start_learning=1", function(data) {
         $('.info').innerText = "Осуществляется запуск обучения сети...";
@@ -9,5 +49,11 @@ function load() {
         $.getJSON(serverAddress + "?start_learning=2", function (result) {
             $('.info').innerText = "Сеть успешно обучена!!!";
         });
-    });
+    });*/
 }
+
+//Продолжить обучение.
+function continueLearn() {
+
+}
+
